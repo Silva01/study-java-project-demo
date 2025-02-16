63a2d1a61b119bb5a04acb2aa13f278654d0364d
silva01 <silva20102@gmail.com>
2025-02-16 19:59:40 -0300

    feat: Added module to practice lambda and streams with java 21

---
+++ pattern-module/src/main/java/silva/daniel/project/poc/kafka/patternmodule/PatternModuleApplication.java
@@ -0,0 +1,13 @@
+package silva.daniel.project.poc.kafka.patternmodule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatternModuleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PatternModuleApplication.class, args);
+    }
+
+}
---
+++ pattern-module/src/test/java/silva/daniel/project/poc/kafka/patternmodule/PatternModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package silva.daniel.project.poc.kafka.patternmodule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatternModuleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
